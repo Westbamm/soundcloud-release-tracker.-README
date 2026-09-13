cea59bd5501ef89711a20dbc2d81e8a9717e1fa0
Westbamm <paveltenisson@gmail.com>
2026-09-13 21:39:14 +0300

    Fix native MCI path quoting

--- native/MciPlayer.cs
+++ native/MciPlayer.cs
@@ -6,13 +6,14 @@ namespace SoundCloudReleaseTracker;
 internal sealed class MciPlayer : IDisposable
 {
     private const string Alias = "scpreview";
+
     [DllImport("winmm.dll", CharSet = CharSet.Unicode)]
     private static extern int mciSendString(string command, StringBuilder? buffer, int bufferSize, IntPtr hwndCallback);
 
     public void Play(string path)
     {
         Stop();
-        var escaped = path.Replace(""", """");
+        var escaped = path.Replace("\"", "\"\"");
         Check($"open \"{escaped}\" type mpegvideo alias {Alias}");
         Check($"play {Alias}");
     }
