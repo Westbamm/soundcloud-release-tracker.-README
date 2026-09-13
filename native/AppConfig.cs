ec76574660e102ed568b1cd6d79f8bf8da3cb30a
Westbamm <paveltenisson@gmail.com>
2026-09-13 21:52:21 +0300

    Add SoundCloud OAuth redirect URI setting

--- native/AppConfig.cs
+++ native/AppConfig.cs
@@ -6,6 +6,7 @@ namespace SoundCloudReleaseTracker;
 public sealed class AppConfig
 {
     public string ClientId { get; set; } = "";
+    public string RedirectUri { get; set; } = "http://127.0.0.1:8765/callback";
     public List<string> Genres { get; set; } = new() { "House", "Tech House", "Afro House" };
     public int PollMinutes { get; set; } = 15;
     public int LookbackHours { get; set; } = 24;
